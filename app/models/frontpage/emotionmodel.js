b9610c160f87fb3eace8490c15be72dd1182f98f
rkempter <renato.kempter@gmail.com>
2013-01-05 19:21:52 +0100

    emotionmodel comments

--- app/models/frontpage/emotionmodel.js
+++ app/models/frontpage/emotionmodel.js
@@ -12,6 +12,7 @@ define([
         },
 
         getCenterPoint: function() {
+            // Compute a random center point in the limits of the window
             var width = $(window).width();
             var height = $(window).height();
             var x = Math.random() * width;
@@ -20,24 +21,23 @@ define([
             return { "x": x, "y": y };
         },
 
-        clear: function() {
-          this.model.destroy();
-        },
-
         getCurrentEmotionShapePath: function() {
+            // Compute the current EmotionShape
             var dataSet = this.get("dataset");
-            
+            // Compute the first point of the shape
             var firstPoint = util.getPoint(this.get("centerPoint"), dataSet[0].value, 0, 200);
             var pathString = "M "+firstPoint.x+" "+firstPoint.y;
             var previous = firstPoint;
-    
-            for(var i = 1; i < 12; i++) {
-                var currentPoint = util.getPoint(this.get("centerPoint"), dataSet[i].value, i*2*Math.PI / 12, 200);
+            // Independant of the labelnumber -> Get number of labels
+            var totalPoints = Constants.labels.length;
+            // Compute the position of the rest of the points
+            for(var i = 1; i < totalPoints; i++) {
+                var currentPoint = util.getPoint(this.get("centerPoint"), dataSet[i].value, i*2*Math.PI / totalPoints, 200);
                 var pathDiff = util.getRelativePoint(currentPoint, previous);
                 pathString += " l "+pathDiff.x+" "+pathDiff.y;
                 previous = currentPoint;
             }
-    
+            // Close the shape
             pathString += " Z";
               
             return pathString
