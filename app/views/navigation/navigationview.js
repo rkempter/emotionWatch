121db2f67b51454c8d7605f0369869c31ca41779
rkempter <renato.kempter@gmail.com>
2013-01-05 18:40:34 +0100

    Navigationview refactored, commented and close method added

--- app/views/navigation/navigationview.js
+++ app/views/navigation/navigationview.js
@@ -16,84 +16,41 @@ define([
         template: 'navbar',
 
         events: {
-            'click #search-form .search-btn': 'triggerSearch',
-            'click #event-form .search-event-btn': 'triggerEventSearch',
-            'click #start-watch': 'triggerStartWatch',
-            'click #stop-watch': 'triggerStopWatch',
-            'click #search-hashtag #hashtag-search-button': 'triggerHashtagSearch',
             'click #search-keyword-btn': 'triggerSearchKeywordModal',
             'click #search-event-btn': 'triggerSearchEventModal',
             'click #settings-btn': 'triggerSettingsModal',
         },
 
         initialize: function(options) {
+            // Load Settings, Event Search and Keyword search views into modals
             this.insertViews({
                 '#search-event .modal-body': new searchEventView(),
                 '#search-keyword .modal-body': new searchKeywordView(),
                 '#settings-modal .modal-body': new settingsView(options),
             });
+
+            this.listenTo(app, 'close', this.close);
         },
 
+        // Show event search modal window when triggered
         triggerSearchEventModal: function(e) {
             $('#search-event').modal('show');
         },
 
+        // Show setting modal window when triggered
         triggerSettingsModal: function(e) {
             $('#settings-modal').modal('show');
         },
 
+        // Show keyword search modal window when triggered
         triggerSearchKeywordModal: function(e) {
             $('#search-keyword').modal('show');
         },
 
-        triggerSearch: function(event) {
-            event.preventDefault();
-            app.paper.clear();
-            this.keyword = $('#search-form #keyword').val();
-            this.startDateTime = $('#search-form select option:selected').attr('value');
-
-            var route = Backbone.history.fragment;
-
-            // app.route.navigate()
-        },
-
-        triggerEventSearch: function(event) {
-            event.preventDefault();
-            app.paper.clear();
-            this.keyword = $('#search-event option:selected').attr('data-hashtag');
-            this.startDateTime = $('#search-event option:selected').attr('data-startDateTime');
-            this.endDateTime = $('#search-event option:selected').attr('data-endDateTime');
-            this.network = $('#search-event #network-event-selector option:selected ').attr('value');
-
-            var route = Backbone.history.fragment;
-
-            // app.route.navigate()
-        },
-
-        triggerHashtagSearch: function(event) {
-            event.preventDefault();
-            app.paper.clear();
-            this.keyword = $('#search-hashtag #hashtag-field').attr('value');
-            this.startDateTime = $('#search-hashtag #start-date-time').attr('value');
-            this.endDateTime = $('#search-hashtag #end-date-time').attr('value');
-            this.network = $('#search-hashtag #network option:selected').attr('value');
-
-            var route = Backbone.history.fragment;
-
-            // app.route.navigate
-        },
-
-        triggerStartWatch: function() {
-            app.trigger('start:watch');
-        },
-
-        triggerStopWatch: function() {
-            app.trigger('stop:watch');
-        },
-
-        clear: function() {
-          this.model.destroy();
-        },
+        close: function() {
+            this.remove();
+            this.unbind();
+        }
     });
 
     return navigationView;
