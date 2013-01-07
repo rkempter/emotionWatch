282981ec9ff45b034fe87e6276cc12e2c3788dec
rkempter <renato.kempter@gmail.com>
2013-01-07 22:40:35 +0100

    Null emotion vector

--- app/constants.js
+++ app/constants.js
@@ -31,45 +31,69 @@ define(['backbone'], function(Backbone) {
         emotionShapeFillColor: '#aaaaaa',
         emotionShapeStrokeColor: '#aaaaaa',
 
-        labels: ['Love', 'Pride', 'Surprise', 'Excitement', 'Joy', 'Like', 'Anger', 'Shame', 'Shock', 'Anxiety', 'Sadness', 'Dislike'],
-        // labels: ['Involvement, 'Amusement', 'Pride', 'Happiness', 'Pleasure', 'Love', 'Awe', 'Relief', 'Surprise', 'Nostalgia', 'Pity', 'Sadness', 'Worry', 'Shame', 'Guilt', 'Regret', 'Envy', 'Disgust', 'Contempt', 'Anger'];
+        //labels: ['Love', 'Pride', 'Surprise', 'Excitement', 'Joy', 'Like', 'Anger', 'Shame', 'Shock', 'Anxiety', 'Sadness', 'Dislike'],
+        labels: ['Involvement', 'Amusement', 'Pride', 'Happiness', 'Pleasure', 'Love', 'Awe', 'Relief', 'Surprise', 'Nostalgia', 'Pity', 'Sadness', 'Worry', 'Shame', 'Guilt', 'Regret', 'Envy', 'Disgust', 'Contempt', 'Anger'];
 
         nullEmotion:
             [
-                { emotion: "love",
+                { emotion: "involvement",
+                  value: 0 
+                },
+                { emotion: "amusement",
                   value: 0 
                 },
                 { emotion: "pride",
                   value: 0 
                 },
+                { emotion: "happiness",
+                  value: 0 
+                },
+                { emotion: "pleasure",
+                  value: 0 
+                },
+                { emotion: "love",
+                  value: 0 
+                },
+                { emotion: "awe",
+                  value: 0 
+                },
+                { emotion: "relief",
+                  value: 0 
+                },
                 { emotion: "surprise",
                   value: 0 
                 },
-                { emotion: "excitement",
+                { emotion: "nostalgia",
                   value: 0 
                 },
-                { emotion: "joy",
-                  value: 0 
-                },
-                { emotion: "like",
-                  value: 0 
-                },
-                { emotion: "anger",
-                  value: 0 
-                },
-                { emotion: "shame",
-                  value: 0 
-                },
-                { emotion: "shock",
-                  value: 0 
-                },
-                { emotion: "anxiety",
+                { emotion: "pity",
                   value: 0 
                 },
                 { emotion: "sadness",
                   value: 0 
                 },
-                { emotion: "dislike",
+                { emotion: "worry",
+                  value: 0 
+                },
+                { emotion: "shame",
+                  value: 0 
+                },
+                { emotion: "guilt",
+                  value: 0 
+                },
+                { emotion: "regret",
+                  value: 0 
+                },
+                { emotion: "envy",
+                  value: 0 
+                },
+                { emotion: "disgust",
+                  value: 0 
+                },
+                { emotion: "contempt",
+                  value: 0 
+                },
+                { emotion: "anger",
                   value: 0 
                 }
             ]
