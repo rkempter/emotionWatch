e0c312cd03bdb1e40336bbe133e3e264417037a6
rkempter <renato.kempter@gmail.com>
2013-01-13 14:21:32 +0100

    Bugfixing & special timeview for comparation

---
+++ app/views/time/timeview_compare.js
@@ -0,0 +1,227 @@
+define([
+    "app",
+    "backbone",
+    "jquery",
+    "lodash",
+    "util",
+    "constants",
+    "plugins/jquery.timer"
+
+], function(app, Backbone, $, _, util, Constants) {
+    
+    var timeCompareView = Backbone.View.extend({
+
+        template: 'timetemplate',
+        
+        initialize: function(options) {
+            _.bindAll(this, 'render');
+            
+            this.model = new Backbone.Model();
+
+            options = options || {};
+
+            this.width = app.windowWidth / 2;
+
+            var self = this;
+
+            this.clockMode = options.clockMode || 'active';
+
+            // set the parameters of the current visualization
+            this.model.set('jump', false);
+            this.model.set('startDateTime', options.startDateTime);
+            this.model.set('endDateTime', options.endDateTime);
+            this.model.set('currentDateTime', options.currentDateTime);
+            this.model.set('timeStep', options.timeStep);
+            this.model.set("label", "Stop");
+            this.network = options.network;
+            this.model.set("timeSpan", options.endDateTime.getTime() - options.startDateTime.getTime());
+
+            // On date & time change, template needs to be rerendered!
+            // If the time is not running, we need to start the watch
+            this.listenTo(app, 'change:globalTime', function(dateTime) {
+                var oldDateTime = this.model.get('currentDateTime');
+                var newDateTime = new Date(oldDateTime.getTime() + this.model.get('timeStep')*1000);
+
+                // Check if we do a jump
+                if(newDateTime.getTime() == dateTime.getTime()) {
+                    this.model.set('jump', false);
+                } else {
+                    this.model.set('jump', true);
+                }
+
+                this.model.set('currentDateTime', dateTime);
+                if(this.model.get('interval') === undefined) {
+                    self.startTime();
+                }
+
+                self.model.set("firstDateTime", moment(dateTime).format("Do MMM YYYY HH:mm:ss"));
+                self.model.set("secondDateTime", moment(new Date(dateTime.getTime() + this.model.get('timeStep')*1000)).format("Do MMM YYYY HH:mm:ss"));
+                self.render(self.template);
+            });
+
+            this.listenTo(app, 'start:watch', function() {
+                self.startTime();
+            });
+
+            this.listenTo(app, 'pause:watch', this.pauseTime);
+            this.listenTo(app, 'resume:watch', this.resumeTime);
+
+            this.listenTo(app, 'close', this.close);
+
+            _.bindAll(this);
+            $(document).on('keydown', self.triggerStartStopKey);
+        },
+
+        close: function() {
+            // Stop interval first
+            if(undefined !== this.model.get('interval')) {
+                this.model.get('interval').stop();
+            }
+            this.unbind(); // Unbind all local event bindings
+            this.remove(); // Remove view from DOM
+        },
+
+        pauseTime: function() {
+            console.log('pauseTime')
+            var width = $('.'+this.network+' .time-block').width();
+            this.model.set('label', 'Start');
+            $('.'+this.network+' .time-block').css('width', width+'px');
+            this.model.set('timeState', 'pause');
+            $('.'+this.network+' .time-block').removeClass('is-transitioning');
+            if(undefined !== this.model.get('interval')) {
+                this.model.get('interval').pause();
+            }
+            this.render();
+        },
+
+        resumeTime: function() {
+            console.log('resume');
+            var width = $('.'+this.network+' .time-block').width();
+            this.model.set('timeState', 'running');
+            this.model.set('label', 'Pause');
+            $('.'+this.network+' .time-block').addClass('is-transitioning');
+            $('.'+this.network+' .time-block').css('width', this.model.get('endPosition'));
+            if(undefined !== this.model.get('interval')) {
+                this.model.get('interval').play();
+            }
+            this.render();
+        },
+
+        // Start running the timer. If the current time is bigger than our
+        // interval, the clock needs to be stopped. Every animationDuration, 
+        // the clock triggers a global time change.
+        startTime: function() {
+            var self = this;
+            this.model.set('timeState', 'running');
+            $('.'+this.network+' .time-block').addClass('is-transitioning');
+            
+            if(this.clockMode == 'active') {
+                this.model.set('startInterval', setInterval(function(t) {
+                    app.trigger('start:time');
+                    clearInterval(self.model.get('startInterval'));
+                },1000));
+                
+                if(undefined === this.model.get('interval')) {
+                    var interval = $.timer(function() {
+                        if(self.model.get('currentDateTime').getTime() >= self.model.get('endDateTime').getTime()) {
+                            self.stopTime();
+                            return;
+                        }
+                        // create new time
+                        var currentDateTime = new Date(self.model.get('currentDateTime').getTime()+self.model.get('timeStep')*1000);
+                        app.trigger("change:globalTime", currentDateTime);
+                    }, app.animationDuration, true);
+
+                    this.model.set('interval', interval);
+                } else {
+                    this.model.get('interval').play();
+                }
+            }
+        },
+
+        events: {
+            'click #start-stop-control': 'triggerStartStop',
+        },
+
+        triggerStartStop: function() {
+            if(this.model.get('timeState') == 'running') {
+                this.model.set('timeState', 'pause');
+                app.trigger('pause:watch');
+            } else if(this.model.get('timeState') == 'pause') {
+                this.model.set('timeState', 'running');
+                app.trigger('resume:watch');
+            }
+        },
+
+        triggerStartStopKey: function(event) {
+            if(event.keyCode == 32) {
+                if(this.model.get('timeState') == 'running') {
+                    this.model.set('timeState', 'pause');
+                    this.pauseTime();
+                } else if(this.model.get('timeState') == 'pause') {
+                    this.model.set('timeState', 'running');
+                    this.resumeTime();
+                }
+            }
+        },
+
+        computeFirstPosition: function() {
+            var timeSpan = this.model.get('startDateTime').getTime() + this.model.get('timeStep')*1000 - this.model.get('startDateTime');
+            var position = timeSpan / this.model.get('timeSpan') * this.width;
+            $('.'+this.network+' .time-block').css('width', position+'px');
+        },
+
+        /**
+         * stopTime
+         *
+         * Clears the interval and removes the variable 'interval' from the model.
+         */
+        stopTime: function() {
+            this.model.get('interval').stop();
+            app.trigger('stop:time');
+        },
+
+        // render the template with labe, date and time.
+        render: function(template) {
+            var output = template({ 
+                label: this.model.get("label"), 
+                firstDateTime: this.model.get('firstDateTime'),
+                secondDateTime: this.model.get('secondDateTime')
+            });
+            this.$el.html(output);
+        },
+
+        afterRender: function() {
+            var currentTimeSpan = this.model.get('currentDateTime').getTime() + this.model.get('timeStep')*1000 - this.model.get("startDateTime").getTime();
+            var position = currentTimeSpan / this.model.get('timeSpan') * this.width;
+            if(position > this.width / 2) {
+                $('.'+this.network+' .time-block .dates').css('text-align', 'right').css('right', '45px');
+            }
+            if(this.model.get('timeState') == 'running' && this.model.get('jump') === false) {
+                $('.'+this.network+' .time-block').css('width', position+'px');
+            } else if(this.model.get('timeState') == 'running' && this.model.get('jump') === true) {
+                $('.'+this.network+' .time-block').removeClass('is-transitioning').css('width', position+'px').delay(100).queue(function(next){
+                    $(this).addClass('is-transitioning');
+                    next();
+                });
+            }
+
+            if(position > this.width) {
+                $('.'+this.network+' .current-time-box').addClass('max-right');
+            } else {
+                $('.'+this.network+' .current-time-box').removeClass('max-right');
+            }
+
+            this.model.set('endPoint', position);
+            if(this.model.get('paused') == true) {
+                this.$el.css('-webkit-animation-play-state', 'paused');
+            } else {
+                this.$el.css('-webkit-animation-play-state', 'running');
+            }
+                
+        }
+        
+    });
+
+    return timeCompareView;
+}); 
